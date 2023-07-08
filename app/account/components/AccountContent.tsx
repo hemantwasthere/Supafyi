06a2930aef8cce4d3bc939604f7de5e2e61834b4
Hemant <hrsmart2150@gmail.com>
2023-07-08 13:16:06 +0530

    Modified responsiveness more

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
-import { useUser } from "@/hooks/useUser";
 import Button from "@/components/Button";
 import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { useUser } from "@/hooks/useUser";
 import { postData } from "@/libs/helpers";
 
 const AccountContent = () => {
@@ -29,39 +30,48 @@ const AccountContent = () => {
             });
             window.location.assign(url);
         } catch (error) {
-            if (error) return alert((error as Error).message);
+            console.log(error)
+            if (error) return toast.error((error as Error).message);
         }
         setLoading(false);
     };
 
     return (
         <div className="mb-7 px-6">
-            {!subscription && (
-                <div className="flex flex-col gap-y-4">
-                    <p>No active plan.</p>
-                    <Button
-                        onClick={subscribeModal.onOpen}
-                        className="w-[300px]"
-                    >
-                        Subscribe
-                    </Button>
-                </div>
-            )}
-            {subscription && (
-                <div className="flex flex-col gap-y-4">
-                    <p>You are currently on the
-                        <b> {subscription?.prices?.products?.name} </b>
-                        plan.
+            <div className="flex flex-col gap-y-4">
+                {user && (
+                    <p> Your current session is authenticated using the email address
+                        <b> {user?.email}</b>
                     </p>
-                    <Button
-                        disabled={loading || isLoading}
-                        onClick={redirectToCustomerPortal}
-                        className="w-[300px]"
-                    >
-                        Open customer portal
-                    </Button>
-                </div>
-            )}
+                )}
+                {!subscription && (
+                    <>
+                        <p>No active plan.</p>
+                        <Button
+                            onClick={subscribeModal.onOpen}
+                            className="w-[300px]"
+                        >
+                            Subscribe
+                        </Button>
+                    </>
+                )}
+                {subscription && (
+                    <>
+                        <p>You are currently on the
+                            <b> {subscription?.prices?.products?.name} </b>
+                            plan.
+                        </p>
+                        <Button
+                            disabled={loading || isLoading}
+                            onClick={redirectToCustomerPortal}
+                            className="w-[244px] sm:w-[300px]"
+                        >
+                            Open customer portal
+                        </Button>
+                    </>
+                )}
+            </div>
+
         </div>
     );
 }
--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,17 +3,35 @@
 import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
-import { Song } from "@/types";
 import usePlayer from "@/hooks/usePlayer";
+import { Song } from "@/types";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface MediaItemProps {
     data: Song;
     onClick?: (id: string) => void;
 }
 
+const getCurrentViewportSize = () => {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+}
+
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
     const player = usePlayer();
     const imageUrl = useLoadImage(data);
+    const [vw, setVw] = useState(window.innerWidth < 444);
+
+    const updateVw = () => {
+        setVw(window.innerWidth < 444);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateVw);
+        return () => window.removeEventListener("resize", updateVw);
+    });
 
     const handleClick = () => {
         if (onClick) {
@@ -33,9 +51,11 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
                 />
             </div>
             <div className="flex flex-col gap-y-1 overflow-hidden">
-                <p className="text-white truncate">{data.title}</p>
+                <p className="text-white truncate">
+                    {vw && data.title.length > 20 ? data.title.slice(0, 12) + "..." : data.title}
+                </p>
                 <p className="text-neutral-400 text-sm truncate">
-                    By {data.author}
+                    By {vw && data.author.length > 20 ? data.author.slice(0, 12) + "..." : data.author}
                 </p>
             </div>
         </div>
