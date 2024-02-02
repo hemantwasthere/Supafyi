189ecb1bfde649854d6105bdf9d6dce55b660375
Hemant <hrsmart2150@gmail.com>
2024-02-02 12:58:45 +0530

    updated names

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,46 @@
-import { Figtree } from 'next/font/google'
+import { Figtree } from "next/font/google";
 
-import getSongsByUserId from '@/actions/getSongsByUserId'
-import Sidebar from '@/components/Sidebar'
-import ModalProvider from '@/providers/ModalProvider'
-import SupabaseProvider from '@/providers/SupabaseProvider'
-import ToasterProvider from '@/providers/ToasterProvider'
-import UserProvider from '@/providers/UserProvider'
-import './globals.css'
-import Player from '@/components/Player'
-import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
+import Sidebar from "@/components/Sidebar";
+import ModalProvider from "@/providers/ModalProvider";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+import UserProvider from "@/providers/UserProvider";
+import "./globals.css";
 
-const figtree = Figtree({ subsets: ['latin'] })
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Spotify Clone!',
-  description: 'Listen to music!',
-}
+  title: "Supafyi",
+  description: "Listen to music!",
+};
 
 export const revalidate = 0;
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const userSongs = await getSongsByUserId()
-  const products = await getActiveProductsWithPrices()
+  const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
 
   return (
     <html lang="en">
-      <body className={`${figtree.className} selection:bg-[#78FEE0]  selection:text-gray-800`}>
+      <body
+        className={`${figtree.className} selection:bg-[#78FEE0]  selection:text-gray-800`}
+      >
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider products={products} />
-            <Sidebar songs={userSongs}>
-              {children}
-            </Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
             <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
